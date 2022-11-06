d34bf101a2fa721dcfe569d874ab0b7265f2e64e
Shiv Shah <shivshahla@gmail.com>
2022-11-06 15:22:54 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,28 @@
+//dependencies
+const express = require('express');
+const morgan = require('morgan');
+
+//express app
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+//temporary
+app.listen(3000);
+
+
+//URLs
+app.get('/', (req,res) => {
+    res.redirect('/home');
+});
+
+app.get('/home', (req,res) => {
+    res.render('index', {title: 'Home - Sun Devil Pizza'});
+});
+
+app.get('/login', (req,res) => {
+    res.render('login', {title: 'Login - Sun Devil Pizza'});
+})
